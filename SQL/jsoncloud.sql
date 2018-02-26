9ab316c975479b26fc93c3b1c565820d3097a91f
Hakan ÖZER <hakanozer02@gmail.com>
2018-02-26 19:42:34 +0300

    SQL

--- SQL/jsoncloud.sql
+++ SQL/jsoncloud.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100130
  File Encoding         : 65001
 
- Date: 26/02/2018 18:53:20
+ Date: 26/02/2018 19:42:04
 */
 
 SET NAMES utf8mb4;
@@ -28,6 +28,8 @@ CREATE TABLE `admins`  (
   `amail` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
   `apassword` varchar(32) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
   `remember_me` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `aphone` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `apicpath` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`aid`) USING BTREE,
   UNIQUE INDEX `unique_admin_mail`(`amail`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
@@ -35,8 +37,117 @@ CREATE TABLE `admins`  (
 -- ----------------------------
 -- Records of admins
 -- ----------------------------
-INSERT INTO `admins` VALUES (1, 'Ali', 'Bilmem', 'ali@ali.com', '827ccb0eea8a706c4c34a16891f84e7b', '');
-INSERT INTO `admins` VALUES (2, 'Veli', 'Bilir', 'veli@veli.com', '827ccb0eea8a706c4c34a16891f84e7b', '');
+INSERT INTO `admins` VALUES (1, 'Ali', 'Bilmem', 'ali@ali.com', '827ccb0eea8a706c4c34a16891f84e7b', '', NULL, NULL);
+INSERT INTO `admins` VALUES (2, 'Veli', 'Bilir', 'veli@veli.com', '827ccb0eea8a706c4c34a16891f84e7b', '', NULL, NULL);
+
+-- ----------------------------
+-- Table structure for adress
+-- ----------------------------
+DROP TABLE IF EXISTS `adress`;
+CREATE TABLE `adress`  (
+  `adressid` int(11) NOT NULL AUTO_INCREMENT,
+  `adresstitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `adresscityid` int(255) NULL DEFAULT NULL,
+  `adresstownid` int(11) NULL DEFAULT NULL,
+  `adressneighborhoodid` int(255) NULL DEFAULT NULL,
+  `adressdescription` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `adressinformation` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `adressdistrictid` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`adressid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for campaigns
+-- ----------------------------
+DROP TABLE IF EXISTS `campaigns`;
+CREATE TABLE `campaigns`  (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `ctitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `cdetail` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `cdate` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`cid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `categoryid` int(11) NOT NULL AUTO_INCREMENT,
+  `categorycompanyid` int(11) NULL DEFAULT NULL,
+  `categoryparentid` int(11) NULL DEFAULT NULL,
+  `categorytitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `categorydescription` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `categorysort` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `categorylink` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`categoryid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for companies
+-- ----------------------------
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies`  (
+  `companyid` int(11) NOT NULL AUTO_INCREMENT,
+  `companyname` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `companyphone` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `companyfax` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `companymail` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `companyadressid` int(11) NULL DEFAULT NULL,
+  `companyapikey` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`companyid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for customers
+-- ----------------------------
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers`  (
+  `customerid` int(11) NOT NULL AUTO_INCREMENT,
+  `customercompanyid` int(11) NULL DEFAULT NULL,
+  `customername` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `customersurname` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `customerpassword` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `customermail` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `customerphone` varchar(10) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`customerid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `orderid` int(11) NOT NULL,
+  `ordercustomerid` int(11) NULL DEFAULT NULL,
+  `orderproductids` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `ordercounts` varchar(50) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `ordertotalprice` decimal(10, 2) NULL DEFAULT NULL,
+  `orderstatus` bit(1) NULL DEFAULT NULL,
+  `orderdate` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`orderid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products`  (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `pcid` int(11) NULL DEFAULT NULL,
+  `ptitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `pcatid` int(11) NULL DEFAULT NULL,
+  `pdesc` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `pdetail` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `pprice` decimal(10, 2) NULL DEFAULT NULL,
+  `ptype` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `pcomid` int(11) NULL DEFAULT NULL,
+  `padressesid` int(255) NULL DEFAULT NULL,
+  `platitude` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `plongtitude` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  `pmap` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`pid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_turkish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for sample
