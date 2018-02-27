9608dd9162d50ec515740c0159587a1d25d643a5
semabilgin <bilginsemaa@gmail.com>
2018-02-27 16:27:47 +0300

    Edit

--- src/main/java/com/javawissen/admin/CategoryController.java
+++ src/main/java/com/javawissen/admin/CategoryController.java
@@ -1,8 +1,18 @@
 package com.javawissen.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping (value="/admin")
 public class CategoryController {
 	
 	// yeni kategori ekle, düzenle, listei, silme
 	// Zeki, Sema
-
+	@RequestMapping (value="/category",method = RequestMethod.GET)
+	public String category() {
+		
+		return "admin/category";
+	}
 }
