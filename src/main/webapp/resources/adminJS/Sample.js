f21af3b275165e54e368b941e9fbb1c990bc9e3c
ramazanisik <isikramazann@gmail.com>
2018-02-27 12:27:16 +0300

    Arama İşlemi
    Sample sayfasında "stitle" ve "sdesc" alanlarına göre arama işlemi yapıldı

--- src/main/webapp/resources/adminJS/Sample.js
+++ src/main/webapp/resources/adminJS/Sample.js
@@ -29,3 +29,41 @@ function pageCount() {
 	});
 }
 
+function pageSearch(itemCount) {
+	var ara = $('#ara').val();
+	$.ajax({
+		url : '/jsoncloud/admin/ajaxSampleSearch',
+		type: 'post',
+		data: {'ara': ara,
+			   'itemCount': itemCount},
+		success : function(data) {
+			if (data == "") {
+				alert("Data Getirmede hata oluştu !");
+			}else {
+				
+				pageCountSearch();
+				$('tr[role=sil]').remove();
+				$('tr#rows').after(data);
+				
+			}
+		}
+	});
+}
+
+function pageCountSearch() {
+	var ara = $('#ara').val();
+$.ajax({
+	url : '/jsoncloud/admin/ajaxPageCountSearch',
+	type: 'post',
+	data: {'ara': ara},
+	success : function(data) {
+		if (data == "") {
+			alert("Sayfa Getirmede hata oluştu !");
+		}else {
+			$('ul.pagination li').remove();
+			$('ul.pagination').html(data);
+			//$('ul#pages').html(data);
+		}
+	}
+});
+}
