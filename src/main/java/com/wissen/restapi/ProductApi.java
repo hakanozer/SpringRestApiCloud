adbcaa163840056b90a80f20bf2f63e64184aa79
Hüssam Durak <hussamdurak@gmail.com>
2018-03-09 17:23:46 +0300

    RestApi Products Listing

--- src/main/java/com/wissen/restapi/ProductApi.java
+++ src/main/java/com/wissen/restapi/ProductApi.java
@@ -10,57 +10,78 @@ import org.hibernate.SessionFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import Models.Companies;
 import Models.Products;
 import Utils.HibernateUtil;
 
+@RestController
 public class ProductApi {
 
 	/*
-	
-	Ramazan Iþýk- Hüssam Durak - Enes Taþdemir-Muharrem DÝDÝCÝ
-	statu: fail, true,
-	message: "",
-	1- Tüm ürünleri listele
-	2- Kategori id sine göre ürünleri listele
-	3- Ürünleri count lü listele : 0 ,10
-	4- kampanyalý ürünler
-	5- Kampanyaya id'sine dahil olan ürünler
-	
-	Not: Bütün ürülerin birden fazla resmi olabilir, resmi olmama durumuda var.
-	image:false, image:true
-	images:[
-		{
-			normal:
-			thun:
-		}
-	]
-	
-	*/
-	
-	SessionFactory sf = HibernateUtil.getSessionFactory();
+	 * 
+	 * Ramazan Iþýk- Hüssam Durak - Enes Taþdemir-Muharrem DÝDÝCÝ statu: fail, true,
+	 * message: "", 1- Tüm ürünleri listele 2- Kategori id sine göre ürünleri
+	 * listele 3- Ürünleri count lü listele : 0 ,10 4- kampanyalý ürünler 5-
+	 * Kampanyaya id'sine dahil olan ürünler
+	 * 
+	 * Not: Bütün ürülerin birden fazla resmi olabilir, resmi olmama durumuda var.
+	 * image:false, image:true images:[ { normal: thun: } ]
+	 * 
+	 */
 
+	SessionFactory sf = HibernateUtil.getSessionFactory();
 
 	@RequestMapping(value = "/{apiKey}/campaignsProducts/{campaignid}", method = RequestMethod.GET)
 	public HashMap<String, Object> campaignsProducts(HttpServletRequest req, @PathVariable Integer campaignid,
 			@PathVariable String apiKey) {
 		Session sesi = sf.openSession();
 		HashMap<String, Object> hm = new HashMap<String, Object>();
-
+		@SuppressWarnings("unchecked")
 		List<Companies> lc = sesi.createQuery("from Companies where companyapikey = '" + apiKey + "' ").list();
 		if (lc.size() < 1) {
 			hm.put("status", false);
 			hm.put("message", "Company Apikey Error !");
 		} else {
 			try {
-				List<Products> ls = sesi.createQuery("from Products where productcompanyid = '"+lc.get(0).getCompanyid()+"' and productcampaignid = '"+campaignid+"' ").list();
+				@SuppressWarnings("unchecked")
+				List<Products> ls = sesi.createQuery("from Products where productcompanyid = '"
+						+ lc.get(0).getCompanyid() + "' and productcampaignid = '" + campaignid + "' ").list();
 				hm.put("status", true);
-				hm.put("message", "Campaigns Listing Successful");
+				hm.put("message", "Products Listing Successful");
 				hm.put("admins", ls);
 			} catch (Exception e) {
 				hm.put("status", false);
-				hm.put("message", "Campaigns Listing Failed");
+				hm.put("message", "Products Listing Failed");
+				System.err.println("Hata : " + e);
+
+			}
+		}
+
+		return hm;
+	}
+
+	@RequestMapping(value = "/{apiKey}/campaignsProductsList", method = RequestMethod.GET)
+	public HashMap<String, Object> campaignsProductsList(HttpServletRequest req, @PathVariable String apiKey) {
+		Session sesi = sf.openSession();
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		@SuppressWarnings("unchecked")
+		List<Companies> lc = sesi.createQuery("from Companies where companyapikey = '" + apiKey + "' ").list();
+		if (lc.size() < 1) {
+			hm.put("status", false);
+			hm.put("message", "Company Apikey Error !");
+		} else {
+			try {
+				@SuppressWarnings("unchecked")
+				List<Products> ls = sesi.createQuery("from Products where productcompanyid = '"
+						+ lc.get(0).getCompanyid() + "' and productcampaignid is not null ").list();
+				hm.put("status", true);
+				hm.put("message", "Products Listing Successful");
+				hm.put("admins", ls);
+			} catch (Exception e) {
+				hm.put("status", false);
+				hm.put("message", "Products Listing Failed");
 				System.err.println("Hata : " + e);
 
 			}
@@ -69,4 +90,5 @@ public class ProductApi {
 		return hm;
 	}
 	
+
 }
