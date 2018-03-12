c49a97828002095bf7dbe3f0f86841600a3b824f
fatma simsek <ssimsekffatma@gmail.com>
2018-03-12 16:48:59 +0300

    AdminSettingsController Düzenleme ve CompanyImage Classları
    AdminSettingsController:
    /next requestmapping eklendi
    
    AdminPofileController:
    /edit requestmapping eklendi
    
    CompanyImage Classları:
    3 adet class eklendi
    cmain.js dosyası eklendi
    
    *adminedit.jsp eklendi

---
+++ src/main/java/com/javawissen/imageupload/CompanyImageController.java
@@ -0,0 +1,124 @@
+
+package com.javawissen.imageupload;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import Models.Companyimage;
+import org.apache.commons.io.IOUtils;
+import org.imgscalr.Scalr;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+
+
+@Controller
+@RequestMapping(value = "/admin/company/imageupload")
+@Import(PropertyPlaceholderConfig.class)
+public class CompanyImageController {
+    
+    private static final Logger log = LoggerFactory.getLogger(CompanyImageController.class);
+    
+    @Autowired
+    private CompanyImageDao CompanyimageDao;
+    
+    private String fileUploadDirectory = "";
+    private String path = "tmp";
+    private Integer companyId = -1;
+
+    @RequestMapping(value="/{id}")
+    public String index(@PathVariable(value="id") Integer id,HttpServletRequest req) {
+    	 companyId =(Integer)req.getSession().getAttribute("companyid");
+    	fileUploadDirectory= "/"+path+"/"+ id;
+    	try {
+			File file = new File(fileUploadDirectory);
+	        if (!file.exists()) {
+	            if (file.mkdir()) {
+	            } else {
+	            }
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        log.debug("CompanyImageController home");
+        System.out.println("CompanyImageController home");
+        return Utils.Utils.loginControl(req, "image/companyImage") ;
+    }
+    
+        
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public @ResponseBody Map upload(MultipartHttpServletRequest request, HttpServletResponse response) {
+        log.debug("uploadPost called");
+        Iterator<String> itr = request.getFileNames();
+        MultipartFile mpf;
+        List<Companyimage> list = new LinkedList<>();
+        
+        while (itr.hasNext()) {
+            mpf = request.getFile(itr.next());
+            log.debug("Uploading {}", mpf.getOriginalFilename());
+            
+            String newFilenameBase = UUID.randomUUID().toString();
+            String originalFileExtension = mpf.getOriginalFilename().substring(mpf.getOriginalFilename().lastIndexOf("."));
+            String newFilename = newFilenameBase + originalFileExtension;
+            String storageDirectory = fileUploadDirectory;
+            String contentType = mpf.getContentType();
+            
+            File newFile = new File(storageDirectory + "/" + newFilename);
+            try {
+                mpf.transferTo(newFile);
+                
+                BufferedImage thumbnail = Scalr.resize(ImageIO.read(newFile), 290);
+                String thumbnailFilename = newFilenameBase + "-thumbnail.png";
+                File thumbnailFile = new File(storageDirectory + "/" + thumbnailFilename);
+                ImageIO.write(thumbnail, "png", thumbnailFile);
+                
+               
+                Companyimage image=new Companyimage();
+                
+                image.setName(mpf.getOriginalFilename());
+                image.setCompanyId(companyId);
+                image.setThumbnailFilename(thumbnailFilename);
+                image.setNewFilename(newFilename);
+                image.setContentType(contentType);
+                image.setSize(mpf.getSize());
+                image.setThumbnailSize(thumbnailFile.length());
+                image = CompanyimageDao.create(image);
+                image.setUrl("/picture/"+image.getId());
+                image.setThumbnailUrl("/thumbnail/"+image.getId());
+                image.setDeleteUrl("/delete/"+image.getId());
+                image.setDeleteType("DELETE");
+                list.add(image);
+                
+            } catch(IOException e) {
+                log.error("Could not upload file "+mpf.getOriginalFilename(), e);
+            }
+            
+        }
+        
+        Map<String, Object> files = new HashMap<>();
+        files.put("files", list);
+        return files;
+    }
+}
